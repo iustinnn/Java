f3886abffe0e4c921befd8df8c32cc0bff7c145a
iustinnn <98700219+iustinnn@users.noreply.github.com>
2023-03-28 00:33:18 +0300

    Add files via upload

---
+++ Lab5Homework/AddCommand.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class AddCommand implements Comanda {
+    public AddCommand() {
+
+    }
+
+    @Override
+    public void run(Catalog catalog, Object... args) { //daca avem argumente de tip obiect, avem docs pe care le adaugam
+        if (args.length >= 1) for (Object argument : args) {
+            Document doc = (Document) argument;
+            catalog.docs.add(doc);
+            System.out.println(doc.getId());
+
+        }
+    }
+
+}
