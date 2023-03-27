585af21117b7af1e1c9c93a8c9c05d3617d3cf02
iustinnn <98700219+iustinnn@users.noreply.github.com>
2023-03-28 00:30:18 +0300

    added run method

--- Lab5Homework/ListCommand.java
+++ Lab5Homework/ListCommand.java
@@ -4,17 +4,11 @@ public class ListCommand implements Comanda {
     public ListCommand() {
 
     }
-
-    public ListCommand(Catalog catalog) {
-        for (Document doc : catalog.docs) {
-            System.out.println(doc.getId() + " " + doc.getTitle());
-        }
+public void run(Catalog catalog,Object ... args){
+    for (Document doc : catalog.docs) {
+        System.out.println(doc.getId() + " " + doc.getTitle());
     }
+}
 
-    public void ListCommand(Catalog catalog) {
-        for (Document doc : catalog.docs) {
-            System.out.println(doc.getId() + " " + doc.getTitle());
-        }
-    }
 }
 
