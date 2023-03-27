a813302b93cb03e1c398043ef6dd328fa3a52ef8
iustinnn <98700219+iustinnn@users.noreply.github.com>
2023-03-28 00:27:55 +0300

    added run method (except loadCommand)

--- Lab5Homework/Main.java
+++ Lab5Homework/Main.java
@@ -27,30 +27,33 @@ public class Main {
         SaveCommand saveCommand = new SaveCommand();
         ListCommand lista = new ListCommand();
         ViewCommand viewCommand = new ViewCommand();
-        ShowReport showReport=new ShowReport();
+        ShowReport showReport = new ShowReport();
         Catalog catalog = new Catalog("MyDocuments");
         var book = new Document("article1", "script", "C:\\Users\\pc\\Documents\\script.docx");
-        add.AddCommand(catalog, book);
+        var book7 = new Document("article12", "script2", "D:\\comenzi.sql");
         var book1 = new Document("slides1", "laborator", "https://profs.info.uaic.ro/~acf/java/labs/slides/lab_05.pdf");
-       add.AddCommand(catalog, book1);
-        saveCommand.save(catalog, "C:\\Users\\pc\\Desktop\\Materii\\catalogNou.json");//salveaza la path obiectele din catalog in json
-        lista.ListCommand(catalog);//afiseaza in console id-urile si titlurile documentelor din catalog.
-        viewCommand.view(catalog,book1); //va deschide fereastra web pentru lab5
-        viewCommand.view(catalog,book); //va deschide un articol din PC-ul meu
-showReport.show(catalog);
+        add.run(catalog, book, book1, book7);
+        saveCommand.run(catalog, "C:\\Users\\pc\\Desktop\\Materii\\catalogNouu.json");//salveaza la path obiectele din catalog in json
+        lista.run(catalog);//afiseaza in console id-urile si titlurile documentelor din catalog.
+        viewCommand.run(catalog, book1); //va deschide fereastra web pentru lab5
+        viewCommand.run(catalog, book); //va deschide un articol din PC-ul meu
+        showReport.run(catalog);
 
     }
 
     private void testLoadView() throws InvalidCatalogException, IOException {
         try {
             LoadCommand loadCommand = new LoadCommand();
-            Catalog catalog = loadCommand.load("C:\\Users\\pc\\Documents\\fisierOutput.json");
-        } catch (FileNotFoundException e) {
+            Catalog catalogTemporar=new Catalog();
+            catalogTemporar=loadCommand.run(catalogTemporar, "C:\\Users\\pc\\Desktop\\Materii\\catalogNouu.json");
+            System.out.println("Catalogul loadat tocmai a primit datele" + catalogTemporar.docs);
+        } catch (InvalidCatalogException e) {
             System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Eroare la incarcarea catalogului.");
         }
 
     }
-
 
 
 }
